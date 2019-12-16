372a7c549c8ad3ad1a81a1a2276bada172ea47ac
Brigette LeVert <bnl.levert@gmail.com>
2019-12-16 01:07:06 +0000

    Done.

--- times.rb
+++ times.rb
@@ -1,7 +1,20 @@
-def using_times
+#def using_times
 	#your code here
+#	puts "Wingardium Leviosa"
+
+#end
+
+counter = 0 
+
+#7.times do
+  #def using_times
+  #  puts "Wingardium Leviosa"
+  #  counter += 1 
+  #end
+
+def using_times
+  7.times do 
+    puts "Wingardium Leviosa"
+  end
 end
-
-
-
 
