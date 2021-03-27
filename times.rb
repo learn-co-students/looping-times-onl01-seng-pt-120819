16de7aff4f9c41615228f5a944cdb7bc75724df5
catherinesmiley <catherinesmiley@gmail.com>
2021-03-27 19:13:23 -0400

    Done.

--- times.rb
+++ times.rb
@@ -1,5 +1,42 @@
-def using_times
-  7.times do
+def using_times 
+  7.times do 
     puts "Wingardium Leviosa"
-  end
-end
+  end 
+end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def using_times
+#   7.times do
+#     puts "Wingardium Leviosa"
+#   end
+# end
